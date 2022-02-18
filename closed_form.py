db4282905f4e874a4362f6657df04fcfb74906b1
Austin Joyal <anaustinbeing@gmail.com>
2022-02-17 22:36:02 -0500

    added comments for later reading

--- closed_form.py
+++ closed_form.py
@@ -13,24 +13,31 @@ This method is suitable if:
 import numpy as np
 import matplotlib.pyplot as plt
 
+# sample input data points
 X = 4 * np.random.rand(100, 1)
 y = 5 + 3 * X + np.random.randn(100, 1)
 
-X_with_bias = np.c_[np.ones((100, 1)), X]    # Adding bias term (a column of ones)
+X_with_bias = np.c_[np.ones((100, 1)), X]    # adding bias term (a column of ones)
 
 def find_theta_best(x, y):
     '''
     (XT.X)-1 XTY
     '''
+    # this equation helps us to find the value for theta which is the value
+    # using which we can predict with lowest global mse
     return np.linalg.inv(x.T.dot(x)).dot(x.T).dot(y)
 
 def predict(theta, x):
+    '''
+    X.theta
+    '''
     return x.dot(theta)
 
 theta_best = find_theta_best(X_with_bias, y)
 
 y_pred = predict(theta_best, X_with_bias)
 
-plt.plot(X, y, "b.")
-plt.plot(X, y_pred, "r-")
+plt.plot(X, y, "b.")            # plots input and actual output
+plt.plot(X, y_pred, "r-")       # plots input and the predicted output
+                                # in closed-form, we find the best fit line through the data points.
 plt.show()
